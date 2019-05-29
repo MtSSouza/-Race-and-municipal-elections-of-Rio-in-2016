e54459a3499746d172c046790bf5e493716c194e
Matheus Souza <mathsouza64@gmail.com>
2019-05-29 09:37:44 -0300

    Fix names

--- DescriptiveAnalysis (BRASIL).R
+++ DescriptiveAnalysis (BRASIL).R
@@ -15,18 +15,18 @@ install.packages("memisc")
 library(memisc)
 
 Bd2018final$EleitoNEleito <- recode(Bd2018final$DS_SIT_TOT_TURNO, 
-                                    "ELEITO" <- c("ELEITO", "ELEITO POR MÉDIA", "ELEITO POR QP"),
-                                    "Ñ_ELEITO" <- c("NÃO ELEITO", "SUPLENTE", "2º TURNO", "#NULO#"))
+                                    "ELEITO" <- c("ELEITO", "ELEITO POR M�DIA", "ELEITO POR QP"),
+                                    "N_ELEITO" <- c("N�O ELEITO", "SUPLENTE", "2º TURNO", "#NULO#"))
 
 freq(Bd2018final$EleitoNEleito)
 
-Bd2018final$RaçaCateg <- recode(Bd2018final$DS_COR_RACA,
+Bd2018final$RacaCateg <- recode(Bd2018final$DS_COR_RACA,
                                 "BRANCOS" <- c("BRANCA"),
-                                "NÃO_BRANCOS" <- c("AMARELA", "INDÍGENA", "PARDA", "PRETA"))
-freq(Bd2018final$RaçaCateg)
+                                "N_BRANCOS" <- c("AMARELA", "IND�GENA", "PARDA", "PRETA"))
+freq(Bd2018final$RacaCateg)
 
-Bd2018eleitosraça <- data.frame(subset(Bd2018final, EleitoNEleito == "ELEITO"))
-freq(Bd2018eleitosraça$RaçaCateg)
+Bd2018eleitosraca <- data.frame(subset(Bd2018final, EleitoNEleito == "ELEITO"))
+freq(Bd2018eleitosraca$RacaCateg)
 
 Bd2014csv <- read.csv2("C:/Users/b35489/Downloads/consulta_cand_2014_BRASIL.csv")
 Bd2014clear <- data.frame(subset(Bd2014csv, DS_SITUACAO_CANDIDATURA == "APTO"))
@@ -36,44 +36,44 @@ freq(Bd2014final$DS_COR_RACA)
 freq(Bd2014final$DS_SIT_TOT_TURNO)
 
 Bd2014final$EleitoNEleito <- recode(Bd2014final$DS_SIT_TOT_TURNO, 
-                                    "ELEITO" <- c("ELEITO", "ELEITO POR MÉDIA", "ELEITO POR QP"),
-                                    "Ñ_ELEITO" <- c("NÃO ELEITO", "SUPLENTE", "2º TURNO", "#NULO#"))
+                                    "ELEITO" <- c("ELEITO", "ELEITO POR M�DIA", "ELEITO POR QP"),
+                                    "N_ELEITO" <- c("NAO ELEITO", "SUPLENTE", "2º TURNO", "#NULO#"))
 
 freq(Bd2014final$EleitoNEleito)
 
-Bd2014final$RaçaCateg <- recode(Bd2014final$DS_COR_RACA,
+Bd2014final$RacaCateg <- recode(Bd2014final$DS_COR_RACA,
                                 "BRANCOS" <- c("BRANCA"),
-                                "NÃO_BRANCOS" <- c("AMARELA", "INDÍGENA", "PARDA", "PRETA"))
-freq(Bd2014final$RaçaCateg)
+                                "N_BRANCOS" <- c("AMARELA", "IND�GENA", "PARDA", "PRETA"))
+freq(Bd2014final$RacaCateg)
 
-Bd2014eleitosraça <- data.frame(subset(Bd2014final, EleitoNEleito == "ELEITO"))
-freq(Bd2014eleitosraça$RaçaCateg)
+Bd2014eleitosraca <- data.frame(subset(Bd2014final, EleitoNEleito == "ELEITO"))
+freq(Bd2014eleitosraca$RacaCateg)
 
 install.packages("ggplot2")
 library(ggplot2)
 install.packages("dplyr")
 library(dplyr)
 
-Bd2018eleitosraça %>% 
-  count(RaçaCateg) %>% 
-  mutate(Percentual = n / nrow(Bd2018eleitosraça)*100) -> bdplot
+Bd2018eleitosraca %>% 
+  count(RacaCateg) %>% 
+  mutate(Percentual = n / nrow(Bd2018eleitosraca)*100) -> bdplot
 
-ggplot(bdplot, aes(x = RaçaCateg, y = Percentual)) + geom_bar(stat = "identity", fill = "cyan4")
+ggplot(bdplot, aes(x = RacaCateg, y = Percentual)) + geom_bar(stat = "identity", fill = "cyan4")
 
 Bd2018final %>% 
-  count(RaçaCateg) %>% 
+  count(RacaCateg) %>% 
   mutate(Percentual = n / nrow(Bd2018final)*100) -> bdplot2
 
-ggplot(bdplot2, aes(x = RaçaCateg, y = Percentual)) + geom_bar(stat = "identity", fill="chartreuse4")
+ggplot(bdplot2, aes(x = RacaCateg, y = Percentual)) + geom_bar(stat = "identity", fill="chartreuse4")
 
-Bd2014eleitosraça %>% 
-  count(RaçaCateg) %>% 
-  mutate(Percentual = n / nrow(Bd2014eleitosraça)*100) -> bdplot3
+Bd2014eleitosraca %>% 
+  count(RacaCateg) %>% 
+  mutate(Percentual = n / nrow(Bd2014eleitosraca)*100) -> bdplot3
 
-ggplot(bdplot3, aes(x = RaçaCateg, y = Percentual)) + geom_bar(stat = "identity", fill = "cyan3")
+ggplot(bdplot3, aes(x = RacaCateg, y = Percentual)) + geom_bar(stat = "identity", fill = "cyan3")
 
 Bd2014final %>% 
-  count(RaçaCateg) %>% 
+  count(RacaCateg) %>% 
   mutate(Percentual = n / nrow(Bd2014final)*100) -> bdplot4
 
-ggplot(bdplot4, aes(x = RaçaCateg, y = Percentual)) + geom_bar(stat = "identity", fill="chartreuse3")
+ggplot(bdplot4, aes(x = RacaCateg, y = Percentual)) + geom_bar(stat = "identity", fill="chartreuse3")
